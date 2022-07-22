d5682b2114ccf2ea3432afaa1adb81a6e1762207
Alain Richardt <alain@inferex.com>
2022-07-23 05:20:17 +1200

    Version: 0.0.5

--- inferex/__init__.py
+++ inferex/__init__.py
@@ -1,7 +1,7 @@
 """ Top-level package for Inferex CLI """
 
 __app_name__ = "inferex"
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 
 from inferex.api.client import Client
 from inferex.decorator.inferex import pipeline
